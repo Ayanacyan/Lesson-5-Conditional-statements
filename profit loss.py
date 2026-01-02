7495cb5d65a366f9e6c5936a7bf896ccdf07173b
Ayanacyan <amitpacl@gmail.com>
2026-01-01 21:03:43 -0600

    profit loss

---
+++ profit loss.py
@@ -0,0 +1,9 @@
+actual_cost=float(input("Please enter the Actual Product price:"))
+sale_amount=float(input("Please enter the Selling Price:"))
+
+if(sale_amount>actual_cost):
+    amount= sale_amount - actual_cost
+    print("Total Profit = {0}".format(amount))
+
+else:
+    print("No Profit!!!")
