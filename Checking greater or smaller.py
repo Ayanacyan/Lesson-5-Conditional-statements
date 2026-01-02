aafdef8d1d4cf4f3587cab4151b52917f896bd84
Ayanacyan <amitpacl@gmail.com>
2026-01-01 21:10:41 -0600

    Checking for greater or smaller

---
+++ Checking greater or smaller.py
@@ -0,0 +1,9 @@
+i=int(input("please enter a number"))
+if(i<10):
+    print("i is smaller than 10")
+    print("I am in if Block")
+
+else:
+    print("i is greater than 10")
+    print("I am in else Block")
+print("I am not in if and not in else Block")
