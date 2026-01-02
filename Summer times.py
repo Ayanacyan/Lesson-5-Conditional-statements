3b58384cbab323d63aa6fd2bf05f3933e2534e72
Ayanacyan <amitpacl@gmail.com>
2026-01-02 13:58:31 -0600

    Summer time recomendations

---
+++ Summer times.py
@@ -0,0 +1,13 @@
+print("Summer time recomendations!")
+weather=int(input("Please enter the weather in Farenheit"))
+
+if weather>80:
+    print("The temprature is:", weather)
+    print("It is too hot to go outside!!")
+    print("Stay inside and cool off with a lemonade")
+else:
+    print("The temprature is:", weather)
+    print("The weather is good for a short activity outside")
+    print("Grab your swimsuit and sunscreen, its time for a swim!")
+print("Have a great Summer!")
+
